a4c25a468c7fd4c68248bc4bdd5c4f1f787ec83e
luisdiegocm <ld.conejo@gmail.com>
2015-01-13 20:42:11 +0100

    user resolved

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -11,7 +11,7 @@ function createUser(){
         var email     = encodeURIComponent(document.getElementById('email').value);
         var user_name = encodeURIComponent(document.getElementById('user_name').value);
         var password  = encodeURIComponent(document.getElementById('password').value);
-        url="create.json?email=" + email + "&username=" + user_name + "&password=" + password;
+        url="create.json?email=" + email + "&user_name=" + user_name + "&password=" + password;
         xmlhttp.open('POST',url,true);
 
         xmlhttp.onreadystatechange=function(){
